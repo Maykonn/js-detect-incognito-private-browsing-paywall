911c67402456136989d6b712c9d97c02e1994c09
Maykonn Welington Candido <Maykonn@users.noreply.github.com>
2017-10-23 17:04:01 -0200

    Function created

---
+++ incognito_private_browsing_detector.js
@@ -0,0 +1,100 @@
+function retry(isDone, next) {
+    var current_trial = 0, max_retry = 50, interval = 10, is_timeout = false;
+    var id = window.setInterval(
+        function() {
+            if (isDone()) {
+                window.clearInterval(id);
+                next(is_timeout);
+            }
+            if (current_trial++ > max_retry) {
+                window.clearInterval(id);
+                is_timeout = true;
+                next(is_timeout);
+            }
+        },
+        10
+    );
+}
+
+function isIE10OrLater(user_agent) {
+    var ua = user_agent.toLowerCase();
+    if (ua.indexOf('msie') === 0 && ua.indexOf('trident') === 0) {
+        return false;
+    }
+    var match = /(?:msie|rv:)\s?([\d\.]+)/.exec(ua);
+    if (match && parseInt(match[1], 10) >= 10) {
+        return true;
+    }
+    return false;
+}
+
+function detectPrivateBrowsing(callback) {
+    if(/googlebot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis/i.test(window.navigator.userAgent)) {
+        console.log('botbrowsing');
+        return false;
+    }
+
+    var is_private;
+
+    if (window.webkitRequestFileSystem) {
+        window.webkitRequestFileSystem(
+            window.TEMPORARY, 1,
+            function() {
+                is_private = false;
+            },
+            function(e) {
+                console.log(e);
+                is_private = true;
+            }
+        );
+    } else if (window.indexedDB && /Firefox/.test(window.navigator.userAgent)) {
+        var db;
+        try {
+            db = window.indexedDB.open('test');
+        } catch(e) {
+            is_private = true;
+        }
+
+        if (typeof is_private === 'undefined') {
+            retry(
+                function isDone() {
+                    return db.readyState === 'done' ? true : false;
+                },
+                function next(is_timeout) {
+                    if (!is_timeout) {
+                        is_private = db.result ? false : true;
+                    }
+                }
+            );
+        }
+    } else if (isIE10OrLater(window.navigator.userAgent)) {
+        is_private = false;
+        try {
+            if (!window.indexedDB) {
+                is_private = true;
+            }
+        } catch (e) {
+            is_private = true;
+        }
+    } else if (window.localStorage && /Safari/.test(window.navigator.userAgent)) {
+        try {
+            window.localStorage.setItem('test', 1);
+        } catch(e) {
+            is_private = true;
+        }
+
+        if (typeof is_private === 'undefined') {
+            is_private = false;
+            window.localStorage.removeItem('test');
+        }
+    }
+
+    retry(
+        function isDone() {
+            return typeof is_private !== 'undefined' ? true : false;
+        },
+        function next(is_timeout) {
+            callback(is_private);
+        }
+    );
+}
