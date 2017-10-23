94fae629e5c6ed1d017810512ec401a50eb3b29f
Maykonn Welington Candido <Maykonn@users.noreply.github.com>
2017-10-23 17:05:34 -0200

    Added example

---
+++ example.js
@@ -0,0 +1,8 @@
+detectPrivateBrowsing(function (is_private) {
+    console.log('Is private or incognito?:');
+    console.log(is_private);
+    
+    if(is_private) {
+        // your code here...
+    }
+});
